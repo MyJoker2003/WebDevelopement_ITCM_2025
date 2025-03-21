4fe7a5a10d8a7edee02dfbcdcd130545f375271a
Juan Carlos Anastasio Salas <L21070315@cdmadero.tecnm.mx>
2025-03-21 12:30:42 -0600

    Added Documentacion using JSDoc format Amateur

--- Progracion_Web_2025/React/TutorialCapacitation/tic_tac_toe/src/App.js
+++ Progracion_Web_2025/React/TutorialCapacitation/tic_tac_toe/src/App.js
@@ -10,7 +10,7 @@ import { useState } from 'react';
 
 /**
  * Practica #1 React Tutorial
- * @returns Componente { Juego Basico de Tic Tac Toe }
+ * @returns {JSX} Componente { Juego Basico de Tic Tac Toe }
  */
 
 {/*Programa Tic Tac Toe*/}
@@ -22,16 +22,29 @@ function Game(){
   const xIsNext = currentMove % 2 === 0
   const currentSquares = history[currentMove];
 
+  /**
+   * @description actualiza el historial si ha habido un retroceso y se actualiza
+   * el numero del movimiento
+   * @param {Array} nextSquares 
+   */
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0,currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
+  /**
+   * @description Define el id o key del ultimo movimiento
+   * @param {number} nextMove 
+   */
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
 
+  /**
+   * @description Lista dinamica con base en el historial del movimiento
+   * @returns {JSX} Lista de Buttons de navegacion del juego.
+   */
   const moves = history.map((squares,move)=>{
     let description;
     description = move > 0 ? 'Ir al movimiento #' + move : 'Ir al inicio del juego';
@@ -56,9 +69,10 @@ function Game(){
 }
 
 /**
- * 
- * @param {*} param0 
- * @returns Button Component. Parte del Tablero de Juego con contenido cambiante
+ * @description Parte del Tablero de Juego con contenido cambiante.
+ * @param {number} value
+ * @param {function} onSquareClick
+ * @returns {JSX}Button Component. 
  */
 function Square({value, onSquareClick}){
   return (
@@ -69,18 +83,25 @@ function Square({value, onSquareClick}){
 }
 
 /**
- * @param
- * @returns Tablero de Juego con matriz de 3X3 de Components Square  
+ * @param {boolean} xIsNext Indica el turno del jugador
+ * @param {Array} squares Distribucion de las marcas (X/O) en el tablero
+ * @param {function} onPlay Acciones al marcar una casilla. Funcion de Orden Superior
+ * @returns {JSX}Tablero de Juego con matriz de 3X3 de Components Square  
  */
 function Board({ xIsNext, squares, onPlay }) {
 
+  /**
+   * @description muestra el movimiento que hizo el jugador y cambia el turno.
+   * @param {number} i Identificador del boton presionado
+   */
   function handleClick(i){
+    //si el elemento en squares[i] ya esta ocupado o ya hay ganador.
     if (squares[i] || calculateWinner(squares)) return;
     
-    const nextSquares = squares.slice();
-    nextSquares[i] = xIsNext ? "X" : "O";
+    const nextSquares = squares.slice(); //Se crea una copia del tablero fuente.
+    nextSquares[i] = xIsNext ? "X" : "O"; //Se modifica la copia
     
-    onPlay(nextSquares);
+    onPlay(nextSquares); //Se reemplaza el tablero con la copia modificada.
   }
 
   const winner = calculateWinner(squares);
@@ -109,7 +130,14 @@ function Board({ xIsNext, squares, onPlay }) {
   )
 }
 
+/**
+ * @description Busca si el table actual cumple con las condiciones de vitoria
+ * para cualquiera de los jugaodres.
+ * @param {Array} squares 
+ * @returns {String} El simbolo del jugador ganador o null
+ */
 function calculateWinner(squares){
+  //Combinaciones ganadoras
   const lines = [
     [0,1,2],
     [3,4,5],
@@ -120,13 +148,16 @@ function calculateWinner(squares){
     [0,4,8],
     [2,4,6]
   ];
+  //Se evalua para todas las instrucciones ganadoras
   for (let i = 0; i< lines.length; i++){
     const [a,b,c] = lines[i];
+    //Si la conbinacion ganadora esta ocupada por los mismo elementos
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
         return squares[a];
+        //El simbolo del ganador
     }
   }
-  return null; 
+  return null; //No existe ganador de momento.
 }
 
-export default Game;
+export default Game; //Se exporta la funcion Contenedora principal
